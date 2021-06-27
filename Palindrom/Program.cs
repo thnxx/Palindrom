909ef7666de7e74548908614d4f1d586a796ecdf
acee <acee@DESKTOP-JOM06HL>
2021-06-27 19:58:09 +0300

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            dongu();
+            Console.ReadLine();
+        }
+
+        static void dongu()
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("Kelime giriniz(Cikis:q):");
+                string kelime;
+                kelime = Console.ReadLine();
+                if (kelime!="q")
+                {
+                    kontrolYapisi(kelime);
+                }
+                else
+                {
+                    Console.WriteLine("Cikis yapiliyor...");
+                    break;
+                }
+                
+            }
+            
+        }
+
+        static bool kacKelime(String kelime)
+        {
+            if (kelime.Length <= 2)
+            {
+
+                return false;
+
+            }
+
+
+
+
+            return true;
+
+        }
+
+        static void kontrolYapisi(String kelime)
+        {
+            string tersten = "";
+            if (kacKelime(kelime))
+            {
+                for (int i = 0; i < kelime.Length; i++)
+                {
+                    tersten += kelime[kelime.Length - 1 - i];
+                }
+
+                if (tersten == kelime)
+                {
+                    Console.WriteLine("Palindromdur.Kelime: " + kelime + " " +
+                                      "Tersen: " + tersten);
+                }
+                else
+                {
+                    Console.WriteLine("Palindrom degildir..Kelime: " + kelime + " " +
+                                      "Tersen: " + tersten);
+                }
+            }
+
+        }
+
+
+
+    }
+}
